24ecde14374944c6e37107ce77728faa247cd8b7
Landon Fuller <landonf@FreeBSD.org>
2019-12-04 18:31:56 -0700

    Revert adoption of FLAVORS, add missing dependencies
    FLAVORS are not intended to be used for this type of meta port; using
    flavors like this triggers an 'incorrectly depends on itself' error
    when building with poudriere. See also:
    
        https://github.com/freebsd/poudriere/issues/582
    
    Testing with poudriere also revealed a number of missing dependencies,
    which have now been added.

--- devel/appmenu-gtk-module/files/patch-src_blacklist.c
+++ devel/appmenu-gtk-module/files/patch-src_blacklist.c
@@ -1,8 +1,6 @@
-diff --git a/subprojects/appmenu-gtk-module/src/blacklist.c b/subprojects/appmenu-gtk-module/src/blacklist.c
-index 60f920191a77a60a3614385c3301b8a8f516c9c8..3bbc94aaf03d129487d35b1edc13c95644f6259d 100644
---- src/blacklist.c
+--- src/blacklist.c.orig	2019-05-01 22:08:39 UTC
 +++ src/blacklist.c
-@@ -58,10 +58,7 @@ static bool is_string_in_array(const char *string, GVariant *array)
+@@ -58,10 +58,7 @@ static bool is_string_in_array(const char *string, GVa
  	while (g_variant_iter_loop(&iter, "&s", &element))
  	{
  		if (g_strcmp0(element, string) == 0)
